67afd9da07e3a728d7d69cfbec249a74f48d02d1
nomadlife15u <aaeacd@gmail.com>
2018-12-21 14:33:50 +0900

    model test

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Companies(models.Model):
+    company_name = models.CharField(max_length=30)
+    ticker_code = models.CharField(max_length=6)
